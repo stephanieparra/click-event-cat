feffd6d2082c865a022ea552e220d3659da5163c
stephanieparra <stephanieparrastudio@gmail.com>
2023-03-15 15:38:17 -0700

    created classList event handler block

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    //console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait come back";
+    button.classList.add("disappear");
+  } else {
+    //console.log("No");
+    cat.classList.add("show");
+    button.innerText = "Shoo cat";
+    cat.classList.remove("disappear");
+  }
 });
